ed573b1d3ce59f4a60a2b0800c59a3f03e4cd611
Deepak Shingan <deepak.shingan@gmail.com>
2019-02-01 17:10:20 +0530

    added code for getting linkedIn data

--- jaxrs/oauth/src/main/java/io/codeleaf/authn/jaxrs/oauth/linkedin/LinkedInResource.java
+++ jaxrs/oauth/src/main/java/io/codeleaf/authn/jaxrs/oauth/linkedin/LinkedInResource.java
@@ -2,10 +2,13 @@ package io.codeleaf.authn.jaxrs.oauth.linkedin;
 
 import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.oauth.OAuth20Service;
+import io.codeleaf.common.utils.Strings;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Produces({"application/json"})
@@ -14,6 +17,7 @@ import java.util.concurrent.ExecutionException;
 public final class LinkedInResource {
 
     private final OAuth20Service linkedInService;
+
 
     public LinkedInResource(OAuth20Service linkedInService) {
         this.linkedInService = linkedInService;
@@ -24,10 +28,11 @@ public final class LinkedInResource {
     public Response parseAccessTokenResponse(@QueryParam("code") String code,
                                              @QueryParam("error") String error,
                                              @QueryParam("error_description") String errorDescription,
-                                             @QueryParam("state") String state) throws InterruptedException, ExecutionException, IOException {
+                                             @QueryParam("state") String state) throws InterruptedException, ExecutionException, IOException, URISyntaxException {
         if (code != null && !code.isEmpty() && linkedInService.getState().equals(state)) {
             OAuth2AccessToken accessToken = linkedInService.getAccessToken(code);
-            return Response.status(Response.Status.OK).entity(accessToken).cookie(LinkedInCookie.Factory.create(accessToken)).build();
+            Map<String, Object> map = LinkedInDataProvider.getProfileDetails(accessToken, linkedInService);
+            return Response.status(Response.Status.OK).entity(map).cookie(LinkedInCookie.Factory.create(accessToken, (String) map.get("firstName"), (String) map.get("lastName"), (String) map.get("headline"), Strings.extractHostName(linkedInService.getCallback()))).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).entity("Error :" + error + " Description:" + errorDescription).build();
     }
