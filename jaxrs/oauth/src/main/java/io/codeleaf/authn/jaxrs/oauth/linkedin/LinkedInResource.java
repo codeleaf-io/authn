13e8da4b10417a86b237a9856655e68eaf0142a9
Deepak Shingan <deepak.shingan@gmail.com>
2019-02-03 23:26:36 +0530

    few more temp changes

--- jaxrs/oauth/src/main/java/io/codeleaf/authn/jaxrs/oauth/linkedin/LinkedInResource.java
+++ jaxrs/oauth/src/main/java/io/codeleaf/authn/jaxrs/oauth/linkedin/LinkedInResource.java
@@ -8,14 +8,15 @@ import io.codeleaf.authn.jaxrs.Authentication;
 import io.codeleaf.authn.jaxrs.AuthenticationPolicy;
 import io.codeleaf.common.utils.Strings;
 
+
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import javax.ws.rs.core.MediaType;
 
 @Produces({"application/json"})
 @Consumes({"application/json"})
@@ -40,9 +41,7 @@ public final class LinkedInResource {
         if (code != null && !code.isEmpty() && linkedInService.getState().equals(state)) {
             OAuth2AccessToken accessToken = linkedInService.getAccessToken(code);
             Map<String, Object> map = LinkedInDataProvider.getProfileDetails(accessToken, linkedInService);
-            return Response.status(Response.Status.OK).entity(getEntityString(accessToken))
-                    .cookie(getLinkedInCookie(accessToken, map))
-                    .build();
+            return Response.status(Response.Status.OK).entity(getEntityString(accessToken)).cookie(getLinkedInCookie(accessToken, map)).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).entity("Error :" + error + " Description:" + errorDescription).build();
     }
@@ -56,6 +55,7 @@ public final class LinkedInResource {
 
     @GET
     @Path("/linkedin/profile")
+    @Produces(MediaType.TEXT_HTML)
     @Authentication(value = AuthenticationPolicy.REQUIRED, authenticator = "linkedin")
     public Response getLinkedInProfile() throws NotAuthenticatedException, InterruptedException, ExecutionException, IOException {
         String token = AuthenticationContext.get().getIdentity();
@@ -68,6 +68,7 @@ public final class LinkedInResource {
     }
 
     private String getEntityString(OAuth2AccessToken accessToken) {
-        return "<script type=\"text/javascript\">window.location.href ='http://codeleaf.com</script>";
+       // AuthenticationContext.get().getAttributes().get(LANDING)
+        return "<script type=\"text/javascript\">window.location.href ='http://cybecore.com:3000/sign-in?token="+accessToken.getAccessToken()+"';</script>";
     }
 }
